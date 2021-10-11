100889c6a48a9150f81d3e5196611e3ef04347ab
Plato <platoo@outlook.it>
2021-10-11 16:39:42 +0200

    Use `SelectorEventLoop` for unit tests in a Windows environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+import asyncio
+import sys
+
+import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption(
         "--full",
@@ -10,3 +16,20 @@ def pytest_addoption(parser):
 def pytest_configure(config):
     if not config.option.full and not config.option.markexpr:
         config.option.markexpr = "not slow"
+
+
+# Workaround for known issue of aiohttp on Windows / Python 3.8+
+# See: https://github.com/aio-libs/aiohttp/issues/4324
+@pytest.fixture
+def event_loop():
+    if (
+        sys.version_info[0] == 3
+        and sys.version_info[1] >= 8
+        and sys.platform == "win32"
+    ):
+        loop = asyncio.SelectorEventLoop()
+    else:
+        loop = asyncio.new_event_loop()
+
+    yield loop
+    loop.close()
